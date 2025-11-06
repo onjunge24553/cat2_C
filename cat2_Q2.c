21c0df72dd3b86dcd475d56ec5cc6cd066f64684
onjunge24553 <kelvinnjunge98@gmail.com>
2025-11-06 12:40:23 +0300

    Add wages calculation program
    Initial implementation of a wages calculation program that prompts for hours worked and hourly wage, calculates overtime, and determines tax rates.

---
+++ cat2_Q2.c
@@ -0,0 +1,29 @@
+*/
+muigai kelvin njunge,
+CT101/24553/24,
+6/11/2025,
+PROGRAM:WAGES CALCULATION
+/*
+#include<stdio.h>
+int main(){
+float grosspay;
+int hours_worked_in_a_week;
+float taxes;
+float hourlywage;
+float overtime;
+float taxrate;
+float j,k;
+printf("enter hours_worked_in_ a_ week\n");
+scanf("%d",&hours_worked_in_a_week);
+printf("enter hourlywage\n");
+scanf("%f",&hourlywage);
+for(overtime>40 hours_worked_in_a_week){
+printf("time and a half\n");}
+j=15/100hourlywage;
+k=20/100hourlywage;
+if(taxrate<=600){
+printf("j:%.2f\n",j);}
+else{
+printf("k:%.2f\n",k);}
+return 0;
+}
